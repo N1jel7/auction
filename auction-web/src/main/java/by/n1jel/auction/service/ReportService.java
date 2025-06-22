83072ff771083d30236f6393a014ac2f0a6aa9de
n1jel-laptop <vanojefferson@gmail.com>
2025-06-23 00:05:34 +0300

    New endpoints and adding readme

--- auction-web/src/main/java/by/n1jel/auction/service/ReportService.java
+++
@@ -1,8 +0,0 @@
-package by.n1jel.auction.service;
-
-import by.n1jel.auction.dto.ReportRequestDto;
-import by.n1jel.auction.dto.ReportResponseDto;
-
-public interface ReportService {
-    ReportResponseDto getReport(ReportRequestDto reportRequestDto);
-}
